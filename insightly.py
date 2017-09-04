a2bcd421b2126e3be68773599a4f576a21c52e17
Mohammad Sanjaq <msanjaq@uci.edu>
2017-09-04 02:38:20 -0700

    Added documentation for all current Contact methods

--- insightly.py
+++ insightly.py
@@ -6,15 +6,27 @@ from requests.auth import HTTPBasicAuth
 
 class Contacts:
     def __init__(self, auth):
+        '''
+        Initalizes class
+        :param auth: Authorization needed to use Insightly's API
+        :type auth: requests.auth.HTTPBasicAuth
+
+        '''
         self.auth = auth
         self.base_url = "https://api.insight.ly/v2.2/Contacts"
 
     def get(self, cid):
+        '''
+        Returns the json of a single contact
+
+        :param cid: contact's id
+        :type cid: int
+        '''
         req = requests.get(self.base_url+"/"+str(cid), auth=self.auth)
-        req.raise_for_status()
-        return req.json()
+        return self._get_json(req)
 
     def get_all(self):
+        '''Returns a list of all the contacts'''
         count_url = self.base_url + "?brief=false&top=1&count_total=true"
         count = int(requests.get(count_url,
                                  auth=self.auth).headers["X-Total-Count"])
@@ -29,7 +41,7 @@ class Contacts:
                                                          top)
             req = requests.get(url, auth=self.auth)
 
-            if req.status_code == 400:
+            if req.status_code == 200:
                 pass
 
             elif req.status_code == 429:
@@ -46,12 +58,35 @@ class Contacts:
         return result
 
     def update(self, data):
+        '''
+        Updates a contact and returns it's json post-update
+        
+        :param data: json that will be the result of the update
+        :type data: dict
+        '''
         req = requests.put(self.base_url, json=data, auth=self.auth)
-        req.raise_for_status()
-        return req.json()
+        return self._get_json(req)
 
     def create(self, data):
+        '''
+        Creates a contact and returns it's json post-update
+        
+        :param data: json that will be the result of the update
+        :type data: dict
+        '''
         req = requests.post(self.base_url, json=data, auth=self.auth)
+        return self._get_json(req)
+
+    def _get_json(self, req):
+        '''
+        Raises an error if the request status code is not 200,
+        otherwise returns the json object of the request
+
+        :param req: the request that will have its json checked and retuned
+        :type req: requests.Requests 
+        '''
+        req.raise_for_status()
+        return req.json()
 
 
 class Insightly:
